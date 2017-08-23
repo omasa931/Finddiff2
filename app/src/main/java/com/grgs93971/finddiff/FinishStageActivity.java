471daa0e3b6f85b5c0f83cfc835c744a1261a434
omasa931 <Funazou7967>
2017-08-24 00:30:47 +0900

    commit

--- app/src/main/java/com/grgs93971/finddiff/FinishStageActivity.java
+++ app/src/main/java/com/grgs93971/finddiff/FinishStageActivity.java
@@ -2,7 +2,10 @@ package com.grgs93971.finddiff;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,21 +13,54 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 public class FinishStageActivity extends Activity {
 
     private final String TAG = "FinishStageActivity:";
+    private InterstitialAd interstitial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finishstage);
 
+
+        interstitial = new InterstitialAd(this);
+        String  ad = getResources().getString(R.string.interstitial_ad_unit_id);
+        interstitial.setAdUnitId(ad);
+        interstitial.loadAd(admobUtil.getAdRequest());
+        interstitial.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                displayInterstitial();
+            }
+        });
         ImageButton toTopBtn = (ImageButton)findViewById(R.id.toTop);
         toTopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+//                if (appInstalledOrNot(getApplicationContext(), "com.twitter.android")) {
+//                    String url = "twitter://post?message=test";
+//                    Intent intent = new Intent(Intent.ACTION_VIEW);
+//                    intent.setData(Uri.parse(url));
+//                    startActivity(intent);
+//                } else {
+//                    Log.d("TAG", "Twitter is not");
+//                }
+
                 Intent intent = new Intent(getApplication(), MainActivity.class);
                 startActivity(intent);
+                if (interstitial.isLoaded()) {
+                    interstitial.show();
+                } else if (interstitial.isLoading()) {
+                    Log.d("TAG", "The interstitial is loading.");
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
             }
         });
 
@@ -34,11 +70,29 @@ public class FinishStageActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), SelectStageActivity.class);
                 startActivity(intent);
-
             }
         });
+
+
+    }
+    // インタースティシャルを表示する準備ができたら、displayInterstitial() を呼び出す。
+    public void displayInterstitial() {
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
     }
 
+    boolean appInstalledOrNot(Context context, String uri) {
+        PackageManager pm = context.getPackageManager();
+        boolean app_installed = false;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed ;
+    }
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
         if (event.getAction()==KeyEvent.ACTION_DOWN) {
@@ -63,6 +117,7 @@ public class FinishStageActivity extends Activity {
     protected void onStart() {
         Log.i(TAG, "onStart()");
         super.onStart();
+
     }
 
     @Override
